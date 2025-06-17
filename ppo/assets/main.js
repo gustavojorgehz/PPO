40621cdc615802c0ac7c3b3f38746090840327e3
Gustavo Jorge <gustavojorgehz@gmail.com>
2025-06-17 14:46:47 -0300

    Add "historias" in main

---
+++ ppo/assets/main.js
@@ -0,0 +1,20 @@
+const pessoas = [{
+    nome: "Hedy Lamarr",
+    id:1
+},
+
+{
+    nome:"Ada Lovelace",
+    id:2
+},
+
+{
+    nome:"Radia Perlman",
+    id:3
+},
+
+{
+    nome:"Marissa Mayer",
+    id:4
+}
+]
